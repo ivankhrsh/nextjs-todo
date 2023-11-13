e9d3c1274fe04258aeb69f06d524c687e4d2a31e
Ivan Khoroshylov <host513@gmail.com>
2023-11-13 13:47:15 +0200

    render todos at client side

--- app/firebase/firebaseActions.ts
+++ app/firebase/firebaseActions.ts
@@ -1,26 +1,27 @@
+// 'use server'
 import { Timestamp, addDoc, collection, deleteDoc, doc, getDocs, onSnapshot, orderBy, query, updateDoc } from "firebase/firestore";
 import { db } from "./firebaseClient";
 import { revalidatePath } from "next/cache";
 import { UpdateTodo } from "../types/updateTodo.type";
-import { initAdmin } from "./firebaseAdmin";
-import { getFirestore } from "firebase-admin/firestore";
+// import { initAdmin } from "./firebaseAdmin";
+// import { getFirestore } from "firebase-admin/firestore";
 
-export const getTodosAdminSDK = async () => {
-  await initAdmin();
-  const firestore = getFirestore();
-  const todosQuery = query(collection(db, 'todos'), orderBy('createdAt', 'desc'));
-  const todosSnapshot = await getDocs(todosQuery);
-  const documents = todosSnapshot.docs.map((todo) => {
-  return {
-    title: todo.data().title,
-    description: todo.data().description,
-    completed: todo.data().completed,
-    createdAt:  todo.data().createdAt.toString(),
-    id: todo.id,
-  }});
+// export const getTodosAdminSDK = async () => {
+//   await initAdmin();
+//   const firestore = getFirestore();
+//   const todosQuery = query(collection(db, 'todos'), orderBy('createdAt', 'desc'));
+//   const todosSnapshot = await getDocs(todosQuery);
+//   const documents = todosSnapshot.docs.map((todo) => {
+//   return {
+//     title: todo.data().title,
+//     description: todo.data().description,
+//     completed: todo.data().completed,
+//     createdAt:  todo.data().createdAt.toString(),
+//     id: todo.id,
+//   }});
  
-  return documents;
-};
+//   return documents;
+// };
 
 
 export const deleteTodo = async (todoId: string) => {
